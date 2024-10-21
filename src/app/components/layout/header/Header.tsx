a7bf42f288b7a394889e4bf57e8fbd6b9ce8735e
Nursadyk <tugolbajuulunursadyk@gmail.com>
2024-10-21 10:56:12 +0600

    adding adaptation main-menu

--- src/app/components/layout/header/Header.tsx
+++ src/app/components/layout/header/Header.tsx
@@ -1,83 +1,83 @@
-'use client'
-import { useLanguageStore } from '@/shared/stores/language-store'
-import Dropdown from '@/shared/ui/Dropdown'
-import clsx from 'clsx'
-import { CiSearch } from 'react-icons/ci'
-import scss from './Header.module.scss'
+"use client";
+import { useLanguageStore } from "@/shared/stores/language-store";
+import Dropdown from "@/shared/ui/Dropdown";
+import clsx from "clsx";
+import { CiSearch } from "react-icons/ci";
+import scss from "./Header.module.scss";
 
 const Header = () => {
-	const { $t, language, setLanguage } = useLanguageStore()
-	const menuItems = $t<Record<'title' | 'href', string>[]>(
-		'header.menu',
-		'global'
-	)
-	const languages = $t<string[]>('header.languages', 'global')
-	return (
-		<header id={scss.Header}>
-			<div className='container'>
-				<div className={scss.header}>
-					<div className={scss.header_logo}>
-						<h1>Restaurant</h1>
-					</div>
-					<div className={scss.header_search}>
-						<nav>
-							{Array.isArray(menuItems) &&
-								menuItems?.map(item => (
-									<a href={item.href} key={item.title}>
-										{item.title}
-									</a>
-								))}
-						</nav>
-						<div className={scss.end}>
-							<div className={scss.header_input}>
-								<label className='inlineFlexCenter' htmlFor='search'>
-									{<CiSearch strokeWidth={2} />}
-								</label>
-								<input
-									type='text'
-									id='search'
-									name='search'
-									placeholder={`${$t('header.searchPlaceholder', 'global')}...`}
-								/>
-							</div>
-							<Dropdown
-								pt='centered'
-								isOutsideClick
-								className={scss.dropdown}
-								trigger={({ toggle, ref }) => (
-									<button
-										className={clsx(scss.trigger_btn)}
-										onClick={toggle}
-										ref={ref}
-									>
-										{language}
-									</button>
-								)}
-							>
-								<ul className={scss.languages}>
-									{Array.isArray(languages) &&
-										languages?.map((item, idx) => {
-											const lang = item.split(' - ')[0]?.toLowerCase()
-											return (
-												<li key={`${item}${idx}`}>
-													<button
-														onClick={() => {
-															setLanguage(lang as TypeLanguage)
-														}}
-													>
-														{item}
-													</button>
-												</li>
-											)
-										})}
-								</ul>
-							</Dropdown>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
-	)
-}
+  const { $t, language, setLanguage } = useLanguageStore();
+  const menuItems = $t<Record<"title" | "href", string>[]>(
+    "header.menu",
+    "global"
+  );
+  const languages = $t<string[]>("header.languages", "global");
+  return (
+    <header id={scss.Header}>
+      <div className="container">
+        <div className={scss.header}>
+          <div className={scss.header_logo}>
+            <h1>Restaurant</h1>
+          </div>
+          <div className={scss.header_search}>
+            <nav>
+              {Array.isArray(menuItems) &&
+                menuItems?.map((item) => (
+                  <a href={item.href} key={item.title}>
+                    {item.title}
+                  </a>
+                ))}
+            </nav>
+            <div className={scss.end}>
+              <div className={scss.header_input}>
+                <label className="inlineFlexCenter" htmlFor="search">
+                  {<CiSearch strokeWidth={2} />}
+                </label>
+                <input
+                  type="text"
+                  id="search"
+                  name="search"
+                  placeholder={`${$t("header.searchPlaceholder", "global")}...`}
+                />
+              </div>
+              <Dropdown
+                pt="centered"
+                isOutsideClick
+                className={scss.dropdown}
+                trigger={({ toggle, ref }) => (
+                  <button
+                    className={clsx(scss.trigger_btn)}
+                    onClick={toggle}
+                    ref={ref}
+                  >
+                    {language}
+                  </button>
+                )}
+              >
+                <ul className={scss.languages}>
+                  {Array.isArray(languages) &&
+                    languages?.map((item, idx) => {
+                      const lang = item.split(" - ")[0]?.toLowerCase();
+                      return (
+                        <li key={`${item}${idx}`}>
+                          <button
+                            onClick={() => {
+                              setLanguage(lang as TypeLanguage);
+                            }}
+                          >
+                            {item}
+                          </button>
+                        </li>
+                      );
+                    })}
+                </ul>
+              </Dropdown>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/app/components/page/home/sections/modern/ModernInterior.tsx
+++ src/app/components/page/home/sections/modern/ModernInterior.tsx
@@ -1,56 +1,56 @@
-'use client'
-import interiorImg1 from '@/shared/assets/scril.png'
-import interiorImg2 from '@/shared/assets/scrol2.png'
-import interiorImg3 from '@/shared/assets/scrol3.png'
-import interiorImg4 from '@/shared/assets/scrol4.png'
-import interiorImg5 from '@/shared/assets/scrol5.png'
-import { useLanguageStore } from '@/shared/stores/language-store'
-import SectionTitles from '@/shared/ui/title/SectionTitle'
-import Image from 'next/image'
-import scss from './ModernInterior.module.scss'
+"use client";
+import interiorImg1 from "@/shared/assets/scril.png";
+import interiorImg2 from "@/shared/assets/scrol2.png";
+import interiorImg3 from "@/shared/assets/scrol3.png";
+import interiorImg4 from "@/shared/assets/scrol4.png";
+import interiorImg5 from "@/shared/assets/scrol5.png";
+import { useLanguageStore } from "@/shared/stores/language-store";
+import SectionTitles from "@/shared/ui/title/SectionTitle";
+import Image from "next/image";
+import scss from "./ModernInterior.module.scss";
 const ModernInterior = () => {
-	const { $t } = useLanguageStore()
-	return (
-		<>
-			<section data-scrool='true' id='interior' className={scss.ModernInterior}>
-				<SectionTitles
-					title={$t(
-						'homeSections.moderninterior.sectiontitles.title',
-						'global'
-					)}
-					subtitle=''
-				/>
-				<div className='container'>
-					<div className={scss.ModernInterior__inner}>
-						<div className={scss.modern__content}>
-							<div className={scss.interior__left}>
-								<Image src={interiorImg1} alt='not found' />
-							</div>
-							<div className={scss.interior__right}>
-								<Image src={interiorImg2} alt='not found' />
-								<div className={scss.interior__right__bottom}>
-									<Image src={interiorImg3} alt='not found' />
-									<Image src={interiorImg4} alt='not found' />
-								</div>
-							</div>
-						</div>
-						<div className={scss.modern__content}>
-							<div className={scss.interior__left}>
-								<Image src={interiorImg5} alt='not found' />
-							</div>
-							<div className={scss.interior__right}>
-								<Image src={interiorImg2} alt='not found' />
-								<div className={scss.interior__right__bottom}>
-									<Image src={interiorImg3} alt='not found' />
-									<Image src={interiorImg4} alt='not found' />
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-		</>
-	)
-}
+  const { $t } = useLanguageStore();
+  return (
+    <>
+      <section data-scrool="true" id="interior" className={scss.ModernInterior}>
+        <SectionTitles
+          title={$t(
+            "homeSections.moderninterior.sectiontitles.title",
+            "global"
+          )}
+          subtitle=""
+        />
+        <div className="container">
+          <div className={scss.ModernInterior__inner}>
+            <div className={scss.modern__content}>
+              <div className={scss.interior__left}>
+                <Image src={interiorImg1} alt="not found" />
+              </div>
+              <div className={scss.interior__right}>
+                <Image src={interiorImg2} alt="not found" />
+                <div className={scss.interior__right__bottom}>
+                  <Image src={interiorImg3} alt="not found" />
+                  <Image src={interiorImg4} alt="not found" />
+                </div>
+              </div>
+            </div>
+            <div className={scss.modern__content}>
+              <div className={scss.interior__left}>
+                <Image src={interiorImg5} alt="not found" />
+              </div>
+              <div className={scss.interior__right}>
+                <Image src={interiorImg2} alt="not found" />
+                <div className={scss.interior__right__bottom}>
+                  <Image src={interiorImg3} alt="not found" />
+                  <Image src={interiorImg4} alt="not found" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default ModernInterior
+export default ModernInterior;
