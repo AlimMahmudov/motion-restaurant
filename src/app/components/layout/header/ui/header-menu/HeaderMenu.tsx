926ce47a53bf34e3003cb0476477d48ca9cf0353
Janybekov2007 <islam.janybekov.007@gmail.com>
2024-10-26 20:57:01 +0600

    fix header.menu

--- src/app/components/layout/header/ui/header-menu/HeaderMenu.tsx
+++ src/app/components/layout/header/ui/header-menu/HeaderMenu.tsx
@@ -22,98 +22,99 @@ const HeaderMenu: React.FC<IHeaderMenuProps> = memo(
 		}, ref)
 		const onClose = useCallback(() => set(false), [set])
 		const pathname = usePathname()
-
+		useClickAway(onClose, ref)
 		return (
-				<div className={clsx('relative inlineFlexCenter', scss.header_menu)}>
-					<button
-						onClick={toggle}
-						className={clsx(scss['open_menu'], 'inlineFlexCenter')}
-					>
-						{!state ? (
-							<svg
-								width='34'
-								height='34'
-								viewBox='0 0 34 34'
-								fill='none'
-								xmlns='http://www.w3.org/2000/svg'
-							>
-								<path
-									d='M17 6.375H29.75'
-									stroke='white'
-									strokeWidth='1.5'
-									strokeLinecap='round'
-									strokeLinejoin='round'
-								/>
-								<path
-									d='M17 13.4583H29.75'
-									stroke='white'
-									strokeWidth='1.5'
-									strokeLinecap='round'
-									strokeLinejoin='round'
-								/>
-								<path
-									d='M4.25 20.5417H29.75'
-									stroke='white'
-									strokeWidth='1.5'
-									strokeLinecap='round'
-									strokeLinejoin='round'
-								/>
-								<path
-									d='M4.25 27.625H29.75'
-									stroke='white'
-									strokeWidth='1.5'
-									strokeLinecap='round'
-									strokeLinejoin='round'
-								/>
-							</svg>
-						) : (
-							<IoClose />
-						)}
-					</button>
-					{state && (
-						<motion.div
-							initial={{ opacity: 0, y: 10 }}
-							animate={{ opacity: 1, y: 0 }}
-							exit={{ opacity: 0, y: 10 }}
-							transition={{ duration: 0.2, ease: 'backIn' }}
-							className={clsx(scss.content, 'absolute', {})}
+			<div className={clsx('relative inlineFlexCenter', scss.header_menu)}>
+				<button
+					onClick={toggle}
+					className={clsx(scss['open_menu'], 'inlineFlexCenter')}
+				>
+					{!state ? (
+						<svg
+							width='34'
+							height='34'
+							viewBox='0 0 34 34'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'
 						>
-							<nav className={scss.nav_items}>
-								{Array.isArray(menuItems) &&
-									menuItems?.map(item => {
-										const href = pathname === '/' ? item.href : `/${item.href}`
-										return (
-											<Link onClick={onClose} href={href} key={item.title}>
-												{item.title}
-											</Link>
-										)
-									})}
-							</nav>
-							<ul className={scss.languages}>
-								{Array.isArray(languages) &&
-									languages?.map((item, idx) => {
-										const lang = item.split(' - ')[0]?.toLowerCase()
-										return (
-											<li key={`${item}${idx}`}>
-												<button
-													onClick={() => {
-														setLanguage(lang as TypeLanguage)
-														onClose()
-													}}
-													className={clsx({
-														[scss.active]: language == lang
-													})}
-												>
-													{lang.toUpperCase()}
-												</button>
-											</li>
-										)
-									})}
-							</ul>
-						</motion.div>
+							<path
+								d='M17 6.375H29.75'
+								stroke='white'
+								strokeWidth='1.5'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							/>
+							<path
+								d='M17 13.4583H29.75'
+								stroke='white'
+								strokeWidth='1.5'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							/>
+							<path
+								d='M4.25 20.5417H29.75'
+								stroke='white'
+								strokeWidth='1.5'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							/>
+							<path
+								d='M4.25 27.625H29.75'
+								stroke='white'
+								strokeWidth='1.5'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							/>
+						</svg>
+					) : (
+						<IoClose />
 					)}
-				</div>
-			)
+				</button>
+				{state && (
+					<motion.div
+						initial={{ opacity: 0, y: 10 }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: 10 }}
+						transition={{ duration: 0.2, ease: 'backIn' }}
+						className={clsx(scss.content, 'absolute', {})}
+						ref={ref}
+					>
+						<nav className={scss.nav_items}>
+							{Array.isArray(menuItems) &&
+								menuItems?.map(item => {
+									const href = pathname === '/' ? item.href : `/${item.href}`
+									return (
+										<Link onClick={onClose} href={href} key={item.title}>
+											{item.title}
+										</Link>
+									)
+								})}
+						</nav>
+						<ul className={scss.languages}>
+							{Array.isArray(languages) &&
+								languages?.map((item, idx) => {
+									const lang = item.split(' - ')[0]?.toLowerCase()
+									return (
+										<li key={`${item}${idx}`}>
+											<button
+												onClick={() => {
+													setLanguage(lang as TypeLanguage)
+													onClose()
+												}}
+												className={clsx({
+													[scss.active]: language == lang
+												})}
+											>
+												{lang.toUpperCase()}
+											</button>
+										</li>
+									)
+								})}
+						</ul>
+					</motion.div>
+				)}
+			</div>
+		)
 	}
 )
 export default HeaderMenu
