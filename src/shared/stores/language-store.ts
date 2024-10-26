eec55002c0d17010082feb644c5d54b947d3f486
alim <you@example.com>
2024-10-26 15:21:40 +0600

    fix imports

--- src/shared/stores/language-store.ts
+++ src/shared/stores/language-store.ts
@@ -1,68 +1,47 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-'use client'
+"use client";
 
-import locales from '@/shared/locales'
-import { create } from 'zustand'
+import locales from "@/shared/locales";
+import { create } from "zustand";
 
 interface ILanguageStore {
-	language: TypeLanguage
-	translations: Record<string, Record<string, any>>
-	setLanguage: (lang: TypeLanguage) => void
-	switchLanguage(): void
-	$t: <T>(key: string, ns: string) => T
-	init(): void
+  language: TypeLanguage;
+  translations: Record<string, Record<string, any>>;
+  setLanguage: (lang: TypeLanguage) => void;
+  switchLanguage(): void;
+  $t: <T>(key: string, ns: string) => T;
+  init(): void;
 }
 
 export const useLanguageStore = create<ILanguageStore>()((set, get) => ({
-	language:
-		typeof window !== 'undefined' && localStorage.getItem('lang')
-			? (JSON.parse(localStorage.getItem('lang')!) as TypeLanguage)
-			: 'en',
-	translations: locales,
-	setLanguage: (lang: TypeLanguage) => {
-		set({ language: lang })
-		if (typeof window !== 'undefined') {
-			document.body?.setAttribute('class', lang)
-			localStorage.setItem('lang', JSON.stringify(lang))
-		}
-	},
-	switchLanguage: () => {
-		const currentLanguage = get().language
-		let newLanguage: TypeLanguage
-
-		switch (currentLanguage) {
-			case 'en':
-				newLanguage = 'ru'
-				break
-			case 'ru':
-				newLanguage = 'kg'
-				break
-			case 'kg':
-				newLanguage = 'en'
-				break
-			default:
-				newLanguage = 'en'
-		}
-
-		get().setLanguage(newLanguage)
-	},
-	$t: (key, ns) => {
-		const lang = get().language
-		const translation = get().translations[lang]?.[ns]
-
-		const keys = key.split('.')
-		let result = translation
-
-		for (const k of keys) {
-			result = result?.[k]
-			if (!result) return key
-		}
-
-		return result || key
-	},
-	init() {
-		if (typeof window !== 'undefined') {
-			document.body?.setAttribute('class', get().language)
-		}
-	}
-}))
+  language:
+    typeof window !== "undefined" && localStorage.getItem("lang")
+      ? (JSON.parse(localStorage.getItem("lang")!) as TypeLanguage)
+      : "en",
+  translations: locales,
+  setLanguage: (lang: TypeLanguage) => {
+    set({ language: lang });
+    if (typeof window !== "undefined") {
+      document.body?.setAttribute("class", lang);
+      localStorage.setItem("lang", JSON.stringify(lang));
+    }
+  },
+  switchLanguage: () => {
+    const currentLanguage = get().language;
+    const newLanguage =
+      currentLanguage === "en" ? "ru" : currentLanguage === "ru" ? "kg" : "en";
+    get().setLanguage(newLanguage);
+  },
+  $t: (key, ns) => {
+    const lang = get().language;
+    const translation = get().translations[lang]?.[ns];
+    return (
+      key.split(".").reduce((res, part) => res?.[part], translation) || key
+    );
+  },
+  init: () => {
+    if (typeof window !== "undefined") {
+      document.body?.setAttribute("class", get().language);
+    }
+  },
+}));
--- src/shared/ui/Popup.tsx
+++ src/shared/ui/Popup.tsx
@@ -1,60 +1,71 @@
-'use client'
-import { useKeyPress } from 'ahooks'
-import clsx from 'clsx'
-import React, { memo } from 'react'
-import { createPortal } from 'react-dom'
-import { CgClose } from 'react-icons/cg'
-import { motion } from 'framer-motion'
-interface IPopupProps {
-	className?: string
-	open: boolean
-	onClose(): void
-	keyover?: boolean
-	blur_bg?: boolean
+"use client";
+
+import { useKeyPress } from "ahooks";
+import clsx from "clsx";
+import React, { memo } from "react";
+import { createPortal } from "react-dom";
+import { CgClose } from "react-icons/cg";
+import { motion } from "framer-motion";
+
+interface IPopupProps extends IChildren {
+  className?: string;
+  open: boolean;
+  onClose(): void;
+  keyover?: boolean;
+  blur_bg?: boolean;
 }
 
-type IProps = IPopupProps & IChildren
-export const Popup: React.FC<IProps> = memo(props => {
-	const { className, children, onClose, open, keyover, blur_bg = false } = props
+export const Popup: React.FC<IPopupProps> = memo((props) => {
+  const {
+    className,
+    children,
+    onClose,
+    open,
+    keyover,
+    blur_bg = false,
+  } = props;
 
-	// @ts-ignore
-	useKeyPress(event => {
-		if (event.key === 'Escape' && keyover) {
-			onClose()
-		}
-	})
-	return createPortal(
-		<>
-			{open && blur_bg && (
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{ duration: 0.2, ease: 'backIn' }}
-					onClick={onClose}
-					className={clsx('blur-bg')}
-				/>
-			)}
+  //  @ts-ignore
+  useKeyPress((event: KeyboardEvent) => {
+    if (event.key === "Escape" && keyover) {
+      onClose();
+    }
+  });
 
-			{open && (
-				<motion.div
-					initial={{ opacity: 0, y: 20 }}
-					animate={{ opacity: 1, y: 0 }}
-					exit={{ opacity: 0, y: 20 }}
-					transition={{ duration: 0.2, ease: 'backIn' }}
-					className={clsx('popup', className)}
-				>
-					<div data-popupbody className='popup_body'>
-						<button
-							onClick={onClose}
-							className={clsx('inlineFlexCenter close-popup')}
-						>
-							<CgClose />
-						</button>
-						{children}
-					</div>
-				</motion.div>
-			)}
-		</>,
-		document.body
-	)
-})
+  if (typeof window === "undefined") return null;
+
+  return createPortal(
+    <>
+      {open && blur_bg && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.2, ease: "backIn" }}
+          onClick={onClose}
+          className={clsx("blur-bg")}
+        />
+      )}
+
+      {open && (
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.2, ease: "backIn" }}
+          className={clsx("popup", className)}
+        >
+          <div data-popupbody className="popup_body">
+            <button
+              onClick={onClose}
+              className={clsx("inlineFlexCenter close-popup")}
+            >
+              <CgClose />
+            </button>
+            {children}
+          </div>
+        </motion.div>
+      )}
+    </>,
+    document.body
+  );
+});
