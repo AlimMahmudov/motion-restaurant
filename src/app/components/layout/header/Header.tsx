0d00027de05eb68f67c5aa31c71c06a5525c9db9
Janybekov2007 <islam.janybekov.007@gmail.com>
2024-10-26 21:04:39 +0600

    add header-height var

--- src/app/components/layout/header/Header.tsx
+++ src/app/components/layout/header/Header.tsx
@@ -5,9 +5,10 @@ import scss from './Header.module.scss'
 import HeaderMenu from './ui/header-menu/HeaderMenu'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { memo } from 'react'
+import { memo, useEffect, useRef } from 'react'
 import SearchWithPopup from './ui/search-with-popup/SearchWithPopup'
 import useWindowSize from '@/shared/hooks/useWindowSize'
+import { useSize } from 'ahooks'
 
 const Header = memo(() => {
 	const { $t, language, switchLanguage } = useLanguageStore()
@@ -15,12 +16,23 @@ const Header = memo(() => {
 		'header.menu',
 		'global'
 	)
+	const headerRef = useRef<HTMLHeadElement>(null)
 	const pathname = usePathname()
 	const languages = $t<string[]>('header.languages', 'global')
 	const { width } = useWindowSize()
-
+	const size = useSize(headerRef)
+	useEffect(() => {
+		if (headerRef.current && size) {
+			if (size) {
+				document.documentElement.style.setProperty(
+					'--header-height',
+					`${size?.height}px`
+				)
+			}
+		}
+	}, [headerRef, size])
 	return (
-		<header id={scss.Header}>
+		<header ref={headerRef} id={scss.Header}>
 			<div className='container'>
 				<div className={scss.header}>
 					<Link href='/' className={scss.header_logo}>
