fe843a951f7adf8016870d2d57b100d98ad4cafb
Janybekov2007 <islam.janybekov.007@gmail.com>
2024-10-26 15:29:37 +0600

    fix

--- src/app/components/layout/header/ui/header-menu/HeaderMenu.tsx
+++ src/app/components/layout/header/ui/header-menu/HeaderMenu.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import clsx from 'clsx'
 import { useClickAway, useToggle } from 'ahooks'
 import useWindowSize from '@/shared/hooks/useWindowSize'
+import { IoClose } from 'react-icons/io5'
 interface IHeaderMenuProps {
 	menuItems: Record<'title' | 'href', string>[]
 	languages: string[]
@@ -27,78 +28,80 @@ const HeaderMenu: React.FC<IHeaderMenuProps> = memo(
 						onClick={toggle}
 						className={clsx(scss['open_menu'], 'inlineFlexCenter')}
 					>
-						<svg
-							width='34'
-							height='34'
-							viewBox='0 0 34 34'
-							fill='none'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path
-								d='M17 6.375H29.75'
-								stroke='white'
-								strokeWidth='1.5'
-								strokeLinecap='round'
-								strokeLinejoin='round'
-							/>
-							<path
-								d='M17 13.4583H29.75'
-								stroke='white'
-								strokeWidth='1.5'
-								strokeLinecap='round'
-								strokeLinejoin='round'
-							/>
-							<path
-								d='M4.25 20.5417H29.75'
-								stroke='white'
-								strokeWidth='1.5'
-								strokeLinecap='round'
-								strokeLinejoin='round'
-							/>
-							<path
-								d='M4.25 27.625H29.75'
-								stroke='white'
-								strokeWidth='1.5'
-								strokeLinecap='round'
-								strokeLinejoin='round'
-							/>
-						</svg>
+						{state ? (
+							<svg
+								width='34'
+								height='34'
+								viewBox='0 0 34 34'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<path
+									d='M17 6.375H29.75'
+									stroke='white'
+									strokeWidth='1.5'
+									strokeLinecap='round'
+									strokeLinejoin='round'
+								/>
+								<path
+									d='M17 13.4583H29.75'
+									stroke='white'
+									strokeWidth='1.5'
+									strokeLinecap='round'
+									strokeLinejoin='round'
+								/>
+								<path
+									d='M4.25 20.5417H29.75'
+									stroke='white'
+									strokeWidth='1.5'
+									strokeLinecap='round'
+									strokeLinejoin='round'
+								/>
+								<path
+									d='M4.25 27.625H29.75'
+									stroke='white'
+									strokeWidth='1.5'
+									strokeLinecap='round'
+									strokeLinejoin='round'
+								/>
+							</svg>
+						) : (
+							<IoClose />
+						)}
 					</button>
-					<div
-						className={clsx(scss.content, 'absolute animate transform-yb', {
-							active: state
-						})}
-					>
-						<nav className={scss.nav_items}>
-							{Array.isArray(menuItems) &&
-								menuItems?.map(item => (
-									<Link onClick={onClose} href={item.href} key={item.title}>
-										{item.title}
-									</Link>
-								))}
-						</nav>
-						<ul className={scss.languages}>
-							{Array.isArray(languages) &&
-								languages?.map((item, idx) => {
-									const lang = item.split(' - ')[0]?.toLowerCase()
-									return (
-										<li key={`${item}${idx}`}>
-											<button
-												onClick={() => {
-													setLanguage(lang as TypeLanguage)
-													onClose()
-												}}
-												className={clsx({
-													[scss.active]: language == lang
-												})}
-											>
-												{lang.toUpperCase()}
-											</button>
-										</li>
-									)
-								})}
-						</ul>
-					</div>
+					{state && (
+						<div className={clsx(scss.content, 'absolute', {})}>
+							<nav className={scss.nav_items}>
+								{Array.isArray(menuItems) &&
+									menuItems?.map(item => (
+										<Link onClick={onClose} href={item.href} key={item.title}>
+											{item.title}
+										</Link>
+									))}
+							</nav>
+							<ul className={scss.languages}>
+								{Array.isArray(languages) &&
+									languages?.map((item, idx) => {
+										const lang = item.split(' - ')[0]?.toLowerCase()
+										return (
+											<li key={`${item}${idx}`}>
+												<button
+													onClick={() => {
+														setLanguage(lang as TypeLanguage)
+														onClose()
+													}}
+													className={clsx({
+														[scss.active]: language == lang
+													})}
+												>
+													{lang.toUpperCase()}
+												</button>
+											</li>
+										)
+									})}
+							</ul>
+						</div>
+					)}
 				</div>
 			)
 		)
