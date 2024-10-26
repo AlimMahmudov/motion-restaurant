55023e1838d07180e9407287d92f3b3b32b431f3
alim <you@example.com>
2024-10-26 15:12:39 +0600

    remove useMemo imports

--- src/shared/ui/Image.tsx
+++ src/shared/ui/Image.tsx
@@ -1,63 +1,63 @@
 /* eslint-disable @next/next/no-img-element */
-'use client'
+"use client";
 
-import clsx from 'clsx'
-import Image from 'next/image'
-import React, { memo, useCallback, useMemo, useState } from 'react'
+import clsx from "clsx";
+import Image from "next/image";
+import React, { memo, useCallback, useState } from "react";
 
 interface ImageProps {
-	url: string
-	alt?: string
-	className?: string
-	width: number
-	height: number
+  url: string;
+  alt?: string;
+  className?: string;
+  width: number;
+  height: number;
 }
 
 const CImage: React.FC<ImageProps> = memo(
-	({ url, alt = 'Image', className, width, height }) => {
-		const [isLoading, setIsLoading] = useState<boolean>(true)
-		const [hasError, setHasError] = useState<boolean>(false)
+  ({ url, alt = "Image", className, width, height }) => {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [hasError, setHasError] = useState<boolean>(false);
 
-		const handleLoadingComplete = useCallback(() => {
-			setIsLoading(false)
-		}, [])
+    const handleLoadingComplete = useCallback(() => {
+      setIsLoading(false);
+    }, []);
 
-		const handleError = useCallback(() => {
-			setHasError(true)
-			setIsLoading(false)
-		}, [])
+    const handleError = useCallback(() => {
+      setHasError(true);
+      setIsLoading(false);
+    }, []);
 
-		const formattedUrl = (() => {
-			if (url.startsWith('http://') || url.startsWith('https://')) {
-				return url
-			}
-			return `${window.location.origin}${url}`
-		})()
+    const formattedUrl = (() => {
+      if (url.startsWith("http://") || url.startsWith("https://")) {
+        return url;
+      }
+      return `${window.location.origin}${url}`;
+    })();
 
-		return (
-			<div
-				className={clsx(className, 'inlineFlexCenter custom-image', {
-					loading: isLoading
-				})}
-			>
-				{isLoading && !hasError && <div className='loader v2 small abs'></div>}
-				{!hasError && (
-					<Image
-						src={formattedUrl}
-						alt={alt}
-						onLoad={handleLoadingComplete}
-						onError={handleError}
-						width={width}
-						height={height}
-						loading='lazy'
-					/>
-				)}
-				{hasError && (
-					<div className='error-message'>Ошибка загрузки изображения</div>
-				)}{' '}
-			</div>
-		)
-	}
-)
+    return (
+      <div
+        className={clsx(className, "inlineFlexCenter custom-image", {
+          loading: isLoading,
+        })}
+      >
+        {isLoading && !hasError && <div className="loader v2 small abs"></div>}
+        {!hasError && (
+          <Image
+            src={formattedUrl}
+            alt={alt}
+            onLoad={handleLoadingComplete}
+            onError={handleError}
+            width={width}
+            height={height}
+            loading="lazy"
+          />
+        )}
+        {hasError && (
+          <div className="error-message">Ошибка загрузки изображения</div>
+        )}{" "}
+      </div>
+    );
+  }
+);
 
-export default CImage
+export default CImage;
